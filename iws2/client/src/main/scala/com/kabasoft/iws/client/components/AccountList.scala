2842c680c1e624d1c430515d6bf9054289d0fb64
mady <batexy@gmail.com>
2016-07-01 17:53:49 +0200

    refactored

--- iws2/client/src/main/scala/com/kabasoft/iws/client/components/AccountList.scala
+++ iws2/client/src/main/scala/com/kabasoft/iws/client/components/AccountList.scala
@@ -13,48 +13,39 @@ import scalacss.ScalaCssReact._
 object AccountList {
   @inline private def bss = GlobalStyles.bootstrapStyles
 
-  case class AccountListProps(items: Seq[Account], stateChange: Account => Callback, editItem: Account => Callback, deleteItem: Account => Callback)
+  case class AccountListProps(items: Seq[Account],
+                              editCB: Option[ Account => Callback]=None,
+                              deleteCB:  Option[Account => Callback]=None)
 
   private val AccountList = ReactComponentB[AccountListProps]("AccountList")
     .render_P(p => {
 
       val style = bss.listGroup
       def renderItem(item: Account) = {
-        def editButton =  Button(Button.Props(p.editItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS, bss.buttonOpt(CommonStyle.success))), Icon.edit, "")
-        def deleteButton = Button(Button.Props(p.deleteItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS, bss.buttonOpt(CommonStyle.danger))), Icon.trash, "")
+        def  f(acc:Account):Callback = Callback.empty
+        def  editCB(acc:Account):Callback = p.editCB.getOrElse(f(_))(acc)
+        def  deleteCB(acc:Account):Callback = p.editCB.getOrElse(f(_))(acc)
+        def editButton =  Button(Button.Props(editCB(item), addStyles = Seq(bss.pullRight, bss.buttonXS, bss.buttonOpt(CommonStyle.success))), Icon.edit, "")
+        def deleteButton = Button(Button.Props(deleteCB(item), addStyles = Seq(bss.pullRight, bss.buttonXS, bss.buttonOpt(CommonStyle.danger))), Icon.trash, "")
+        var tag = EmptyTag
+        if((p.deleteCB !=None) && (p.editCB !=None)) tag = List(editButton , deleteButton)
         //List(<.div(bss.formGroup,
         <.li(style.itemOpt(CommonStyle.success),^.fontSize:=12,^.fontWeight:=50,^.maxHeight:=30,^.height:=30, ^.tableLayout:="fixed",
-          /* <.table(bss.buttonWarning,^.className := "table-responsive table-condensed",^.tableLayout:="fixed",
-             <.tbody(
-               //<.tr(bss.formGroup,^.height := 10,//^.fontSize:=10,^.fontWeight:=10,^.maxHeight:=8,
-               <.tr(bss.formGroup,^.height := 20,
-                 <.td( item.id),
-                 <.td( item.description),
-                 <.td( item.dateOfOpen.get.toString),
-                 <.td( "%06.2f".format(item.balance.amount.toDouble)),
-                 //<.td( ^.cls:= "btn btn-default", <.em(^.cls:= "fa fa-edit",Button(Button.Props(p.editItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS)))), ^.paddingLeft:=300),
-                 <.td(Button(Button.Props(p.editItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS,bss.fa_fa_edit))), ^.paddingLeft:=300),
-                 <.td(Button(Button.Props(p.deleteItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS,bss.fa_fa_delete))),^.marginRight:=1)
-               )
-             )
-           )
-          */
 
           <.span(item.id),
           <.span(item.name ,^.paddingLeft:=10),
           <.span(item.description ,^.paddingLeft:=10),
           //<.span(Utils.format(item.dateOfOpen.get),^.paddingLeft:=10),
           <.span("%06.2f".format(item.balance.amount.toDouble),^.paddingLeft:=10),
-          editButton,deleteButton
+          tag
           )
-        //)
-       //)
-
       }
       <.ul(style.listGroup)(p.items.sortBy(_.id) map renderItem)
     })
     .build
+  def apply(items: Seq[Account]) =
+    AccountList(AccountListProps(items))
 
-  def apply(items: Seq[Account], stateChange: Account => Callback, editItem: Account => Callback, deleteItem: Account => Callback) =
-    AccountList(AccountListProps(items, stateChange, editItem, deleteItem))
+  def apply(items: Seq[Account], editCB: Option[Account => Callback], deleteCB: Option[Account => Callback]) =
+    AccountList(AccountListProps(items,  editCB, deleteCB))
 }
