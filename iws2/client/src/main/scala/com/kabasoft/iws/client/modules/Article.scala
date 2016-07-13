cb4d2f8720a6a7cb00e2c07ee688b5be72b1d600
mady <batexy@gmail.com>
2016-07-13 19:38:42 +0200

    removed unused  imports

--- iws2/client/src/main/scala/com/kabasoft/iws/client/modules/Article.scala
+++ iws2/client/src/main/scala/com/kabasoft/iws/client/modules/Article.scala
@@ -12,11 +12,7 @@ import com.kabasoft.iws.gui.macros.Bootstrap._
 import com.kabasoft.iws.gui.macros._
 import com.kabasoft.iws.gui.Utils._
 import com.kabasoft.iws.gui.logger._
-import com.kabasoft.iws.gui.services.{IWSCircuit, RootModel}
-import diode.ModelR
-
-import scalacss.Defaults._
-import scalacss.Attrs._
+import com.kabasoft.iws.gui.services.IWSCircuit
 import scalacss.ScalaCssReact._
 
 
@@ -25,20 +21,15 @@ object ARTICLE {
   @inline private def bss = GlobalStyles.bootstrapStyles
   @inline private def iwsStyles = GlobalStyles.IWSStyles
 
-  import scalacss.Defaults._
-
   implicit def orderingById[A <: Article]: Ordering[A] = {Ordering.by(e => (e.id, e.id))}
   case class Props(proxy: ModelProxy[Pot[Data]])
   case class State(item: Option[Article] = None, name:String)
   class Backend($: BackendScope[Props, State]) {
-    def mounted(props: Props) = {
-      Callback {
+    def mounted(props: Props) = Callback {
         IWSCircuit.dispatch(Refresh(QuantityUnit()))
         IWSCircuit.dispatch(Refresh(Vat()))
         IWSCircuit.dispatch(Refresh(Article()))
      }
-    }
-
 
     def edit(itemx:Option[Article]) =
       $.modState(s => s.copy(item = itemx))
@@ -51,10 +42,8 @@ object ARTICLE {
       }
     }
 
-
     def delete(itemx:Article) =
       $.props >>= (_.proxy.dispatch(Delete(itemx)))
-
 
     def updateId(e: ReactEventI) = {
       val r = e.target.value
@@ -85,8 +74,6 @@ object ARTICLE {
       val vatId = id.substring(0, id.indexOf(":"))
       log.debug(s"VatId  is ${vatId}  ")
       $.modState(s => s.copy(s.item.map( z => z.copy(vat = Some(vatId)))))
-     // $.modState(s => s.copy(item = s.item.map(_.copy(vat = Some(vatId)))))
-
     }
 
     def updateUnit(id:String) = {
@@ -117,9 +104,9 @@ object ARTICLE {
        <.table(^.className := "table-responsive table-condensed", //^.tableLayout := "fixed",
         <.tbody(
           <.tr(bss.formGroup, ^.height := 10.px,
-            buildWItem("id", s.item.map(_.id), "id" , updateId),
-            buildWItem1("name", s.item.map(_.name), "name", updateName),
-            buildWItem("description", s.item.map(_.description), "description", updateDescription)),
+              buildWItem("id", s.item.map(_.id), "id" , updateId),
+              buildWItem1("name", s.item.map(_.name), "name", updateName),
+              buildWItem("description", s.item.map(_.description), "description", updateDescription)),
             <.tr(bss.formGroup, ^.height := 10.px,
               buildWItem("price", s.item.map( x => fmt(x.price.bigDecimal)), "0.0", updatePrice),
               buildWItem1("avg price", s.item.map( x => fmt(x.avgPrice.bigDecimal)), "0.0", updatePrice),
@@ -129,16 +116,12 @@ object ARTICLE {
               buildWItem("Vat", s.item.map(_.vat.getOrElse("Mwst")), "Mwst", evt = noAction),
               buildWItem("packUnit", s.item.map(_.packUnit), "packUnit", evt = noAction)),
              <.tr(bss.formGroup, ^.height := 10.px,
-             buildSItem("group", itemsx = buildIdNameList(items), defValue = "xxxx", evt = updateGroupId),
+              buildSItem("group", itemsx = buildIdNameList(items), defValue = "xxxx", evt = updateGroupId),
               buildSItem("q.unit", itemsx = buildIdNameList(qttyUnit), defValue = "KG", evt = updateUnit),
               buildSItem("Vat", itemsx = buildIdNameList(vat), defValue = "7", evt = updateVat),
-         // <.tr(bss.formGroup, ^.height := 10.px,
-            buildSItem("packUnit", itemsx = buildIdNameList(qttyUnit), defValue = "KG", evt = updatePackUnit))
-
+              buildSItem("packUnit", itemsx = buildIdNameList(qttyUnit), defValue = "KG", evt = updatePackUnit))
           )
-
       )
-
     }
 
     def render(p: Props, s: State) ={
@@ -149,7 +132,6 @@ object ARTICLE {
       def newButton =  Button(Button.Props(edit(Some(Article())), addStyles = Seq(bss.pullRight, bss.buttonXS)), Icon.plusSquare, "New")
 
       val items = article_items.toList.sorted
-      //log.debug(s"itemsitemsitemsitemsitemsitemsitemsitems is ${items} VVVVVVVV ${article_items}")
       Panel(Panel.Props("Article"), <.div(^.className := "panel-heading",^.padding :=0.px), <.div(^.padding :=0.px,
         p.proxy().renderFailed(ex => "Error loading"),
         p.proxy().renderPending(_ > 500, _ => "Loading..."),
@@ -160,7 +142,6 @@ object ARTICLE {
             Some(item => p.proxy.dispatch(Delete[Article](item))))))
        )
       )
-
     }
   }
 
