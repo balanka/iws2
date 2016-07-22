7df6d8ba471841134c8028c66b68728436201bbe
mady <batexy@gmail.com>
2016-07-22 20:36:07 +0200

    refactored

--- iws2/clientmacros/src/main/scala/com/kabasoft/iws/gui/TabAccordionMenu.scala
+++ iws2/clientmacros/src/main/scala/com/kabasoft/iws/gui/TabAccordionMenu.scala
@@ -3,11 +3,53 @@ package com.kabasoft.iws.gui
 import com.kabasoft.iws.gui.AccordionMenu.MenuItem
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
-
 import scala.language.implicitConversions
 
 
 object TabAccordionMenu {
+
+  case class Props(menuItem:MenuItem)
+  val accordionTitleTag = List("data-toggle".reactAttr := "collapse","data-parent".reactAttr :="accordion")
+  val menuTitleTag = List(^.cls := "panel-group", ^.id:="accordion", ^.fontWeight:=10,^.fontSize:=12)
+  val panelTitle:TagMod = (^.cls := "panel-title").compose( ^.fontWeight:=10).compose(^.fontSize:=12)
+
+  def buildMenuItem(item:MenuItem) =
+       <.li(^.className :="list-group-item list-group-item-info",
+             <.div(<.a(^.href:= item.route, item.title))
+           )
+
+  def  buildSubMenu(v1:MenuItem) =
+         <.div(^.className := "panel panel-warning",
+              <.div(^.className := "panel-heading",
+                   <.h4(panelTitle,
+                       <.a(accordionTitleTag,
+                           ^.href:= v1.route,  v1.title)
+                      )
+                  ),
+          <.div(^.className := "panel-collapse collapse", ^.id := v1.title,
+               <.div(^.className :="panel-body", v1.menuItems map buildMenuItem)
+              )
+          )
+
+  def buildMenu(menu:MenuItem) =
+    <.div(^.className := "panel-collapse collapse in",^.id:="accordion1",
+      <.div(^.className :="panel-body", menu.menuItems map buildSubMenu)
+  )
+
+  def buildMainAccordionMenu(menu:MenuItem)= buildMenu(menu)
+
+  val component = ReactComponentB[Props]("Menu")
+    .stateless
+    .render_P(p => {
+      <.div(menuTitleTag, buildMainAccordionMenu(p.menuItem))
+    }).build
+
+  def apply(menuItem:MenuItem) = component(Props(menuItem))
+
+}
+
+
+object TabAcordionMenu2 {
 
   case class Props(menuItem:MenuItem)
   val accordionTitleTag = List("data-toggle".reactAttr := "collapse","data-parent".reactAttr :="accordion")
@@ -24,7 +66,7 @@ object TabAccordionMenu {
   def  buildSubMenu(v1:MenuItem) =
     <.div(^.className := "panel panel-warning",
       <.div(^.className := "panel-heading",
-       // <.span(^.className :="glyphicon glyphicon-th",
+        // <.span(^.className :="glyphicon glyphicon-th",
         <.h4(panelTitle,
           <.a(accordionTitleTag,
             ^.href:= v1.route,  v1.title)
@@ -33,24 +75,23 @@ object TabAccordionMenu {
       //<.div(^.className := "panel-collapse collapse in",^.id:=v1.title,
       <.div(^.className := "panel-collapse collapse",^.id:=v1.title,
         <.div(^.className :="panel-body", v1.menuItems map buildMenuItem)
-        )
+      )
     )
 
   def buildMenu(menu:MenuItem) =
     <.div(^.className := "panel-collapse collapse in",^.id:="accordion1",
-    //<.div(^.className := "panel-collapse collapse",^.id:="accordion1",
-    <.div(^.className :="panel-body",
-       menu.menuItems map buildSubMenu
+      //<.div(^.className := "panel-collapse collapse",^.id:="accordion1",
+      <.div(^.className :="panel-body",
+        menu.menuItems map buildSubMenu
+      )
     )
-  )
 
   def buildMainAccordionMenu(menu:MenuItem)=
     <.div(^.className := "panel panel-primary",
       <.div(^.className := "panel-heading",
-       // <.div(^.className := "fa fa-sitemap",
-         <.h4(panelTitle,
-           <.a( accordionTitleTag,
-              ^.href:= "#accordion1",  menu.title)
+        <.h4(panelTitle,
+          <.a( accordionTitleTag,
+            ^.href:= "#accordion1",  menu.title)
         )
       ),
       buildMenu(menu)
