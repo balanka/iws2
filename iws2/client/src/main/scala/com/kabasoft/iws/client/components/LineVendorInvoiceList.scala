8fcf681073887c64e03faf2799c41374ebc93950
mady <batexy@gmail.com>
2016-07-30 20:13:38 +0200

    minor refactoring  and  fixed bug for editing

--- iws2/client/src/main/scala/com/kabasoft/iws/client/components/LineVendorInvoiceList.scala
+++ iws2/client/src/main/scala/com/kabasoft/iws/client/components/LineVendorInvoiceList.scala
@@ -1,7 +1,6 @@
 package com.kabasoft.iws.client.components
 
 import java.util.Date
-
 import com.kabasoft.iws.gui.StringUtils._
 import com.kabasoft.iws.gui.Utils._
 import com.kabasoft.iws.gui.logger._
@@ -12,7 +11,6 @@ import com.kabasoft.iws.shared._
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 import org.widok.moment.Moment
-
 import scalacss.ScalaCssReact._
 
 object LineVendorInvoiceList {
@@ -27,29 +25,14 @@ object LineVendorInvoiceList {
   class Backend($: BackendScope[Props, State]) {
 
     def mounted(props: Props) = Callback {
-        IWSCircuit.dispatch(Refresh(Article()))
-        IWSCircuit.dispatch(Refresh(QuantityUnit()))
-        IWSCircuit.dispatch(Refresh(Vat()))
+        IWSCircuit.dispatch(Refresh(Account()))
       }
 
-    def edit(line:LineVendorInvoice) = {
-      //log.debug(s" order to edit Line is ${line}")
-      //$.modState(s => s.copy(item = Some(line)).copy(edit = true))
-       $.modState(s => s.copy(item = Some(line)))
-    }
-//    def updateAccount(id: String) = {
-//      val r =id.split(":")
-//      val accountId = r(0)
-//      //val qttyUnitId = r(2)
-//     // val  vatId = r(3)
-//      log.debug(s"ItemId Key is ${r}  itemid  ${accountId}  ")
-//      //$.modState(s => s.copy(item = s.item.map(_.copy(unit=Some(qttyUnitId)))))>>
-//      //  $.modState(s => s.copy(item = s.item.map(_.copy(vat=Some(vatId)))))>>
-//        $.modState(s => s.copy(item = s.item.map(_.copy(account = Some(accountId)))))>>setModfied
-//    }
+    def edit(line:LineVendorInvoice) = $.modState(s => s.copy(item = Some(line)))
+
     def updateAccount(id:String) = {
-      val oaccountId = id.substring(0, id.indexOf("|"))
-      $.modState(s => s.copy(item = s.item.map(_.copy(oaccount = Some(oaccountId)))))>>
+      val accountId = id.substring(0, id.indexOf("|"))
+      $.modState(s => s.copy(item = s.item.map(_.copy(account = Some(accountId)))))>>
         setModfied
     }
 
@@ -59,14 +42,15 @@ object LineVendorInvoiceList {
         setModfied
     }
 
-    def updateSide(id:String) = {
-      val sideId = id.substring(0, id.indexOf("|"))
-      $.modState(s => s.copy(item = s.item.map(_.copy(side = sideId))))>>
+    def updateSide(e: ReactEventI) = {
+      val sideId = e.target.checked
+      //log.debug(s" sideId ${sideId}")
+      $.modState(s => s.copy(item = s.item.map(_.copy(side = sideId)))).runNow() //>>
         setModfied
     }
 
     def updateDuedate(e: ReactEventI) = {
-      log.debug(s" Duedate is  mm${e.target.value}")
+     // log.debug(s" Duedate is  mm${e.target.value}")
       val l = e.target.value
       Moment.locale("de_DE")
       val m = Moment(l)
@@ -84,7 +68,6 @@ object LineVendorInvoiceList {
         setModfied
     }
 
-
     def updateAmount(e: ReactEventI, s1:State) = {
       val l =e.target.value.toDouble
       //log.debug(s"Item price is ${l} State:${s1}")
@@ -92,14 +75,12 @@ object LineVendorInvoiceList {
       setModfied
     }
 
-
-    def delete(line:LineVendorInvoice, deleteLineCallback:LineVendorInvoice =>Callback) =
-      Callback.log(s"LinePurchaseOrder deleted>>>>>  ${line}") >> deleteLineCallback(line)
+    def delete(line:LineVendorInvoice, deleteLineCallback:LineVendorInvoice =>Callback) = deleteLineCallback(line)
+     // Callback.log(s"LinePurchaseOrder deleted>>>>>  ${line}") >> deleteLineCallback(line)
     def save(line:LineVendorInvoice, saveLineCB:LineVendorInvoice =>Callback) = {
       saveLineCB(line) >> $.modState(s => s.copy(item = None).copy(edit = false))
         //>> resetState
     }
-
 
     def postProcess (item:VendorInvoice[LineVendorInvoice]) =  Callback {
       val ro = item.getLines.filter(_.created ==true).map( e => item.replaceLine( e.copy(created = false).copy(modified =false)))
@@ -116,27 +97,23 @@ object LineVendorInvoiceList {
     def render(p:Props, s: State) = {
       val style = bss.listGroup
       val its  = p.porder.lines.getOrElse(Seq.empty[LineVendorInvoice])
-      def items =  IWSCircuit.zoom(_.store.get.models.get(7)).eval(IWSCircuit.getRootModel).get.get.items.asInstanceOf[List[Account]]
-      def qttyUnit =  IWSCircuit.zoom(_.store.get.models.get(4)).eval(IWSCircuit.getRootModel).get.get.items.asInstanceOf[List[QuantityUnit]]
-      def vat =  IWSCircuit.zoom(_.store.get.models.get(5)).eval(IWSCircuit.getRootModel).get.get.items.asInstanceOf[List[Vat]]
+      def items =  IWSCircuit.zoom(_.store.get.models.get(9)).eval(IWSCircuit.getRootModel).get.get.items.asInstanceOf[List[Account]]
 
       def saveButton = Button(Button.Props(save(s.item.getOrElse(LineVendorInvoice()),p.saveLine),
         addStyles = Seq(bss.pullRight, bss.buttonXS, bss.buttonOpt(CommonStyle.success))), Icon.circleO, "")
       def newButton = Button(Button.Props( newLine(LineVendorInvoice(created = true), p.newLine),
         addStyles = Seq(bss.pullRight, bss.buttonXS)), Icon.plusSquare, "")
       def buildIdNameList [A<:Masterfile](list: List[A]): List[String]= list map (iws =>(iws.id+"|"+iws.name))
-      //def buildArticleList [A<:Article](list: List[A]): List[String]= list map (iws =>(iws.id+":"+iws.name +":"+iws.qttyUnit  +":"+iws.vat.getOrElse("0")))
 
       def editFormLine : Seq [TagMod] = List(
           <.div(^.cls :="row",
-            buildSItemN("Account", itemsx = buildIdNameList(items), defValue = "0001", evt = updateAccount, "col-xs-2"),
-            buildSItemN("Side", List("S","H"), defValue = "S", evt = updateSide, "col-xs-2"),
-            buildSItemN("OAccount", itemsx = buildIdNameList(items), defValue = "7", evt = updateOAccount, "col-xs-2"),
+            buildSItemN("Account", itemsx = buildIdNameList(items), defValue = "0001", evt = updateAccount, "col-xs-3"),
+            buildBItem("D/C", s.item.map(_.side), true, evt = updateSide, "col-xs-1" ),
+            buildSItemN("OAccount", itemsx = buildIdNameList(items), defValue = "7", evt = updateOAccount, "col-xs-3"),
             buildWItemN[BigDecimal]("Amount", s.item.map(_.amount), 0.0, updateAmount(_, s),"col-xs-2"),
-
             buildDateN("Duedate", s.item.map(_.duedate.getOrElse(new Date())), new Date(), updateDuedate,"col-xs-2"),
-            //buildWItemN[BigDecimal]("Quantity", s.item.map(_.quantity), 0.0, updateQuantity,"col-xs-2"),
-              saveButton, newButton)
+              saveButton, newButton),
+          <.div(^.cls :="row",buildAreaItem("Text", s.item.map(_.text), "", updateText,"col-xs-12"))
          )
 
       <.div(bss.formGroup,
@@ -157,22 +134,20 @@ object LineVendorInvoiceList {
                             bss.buttonOpt(CommonStyle.success))), Icon.edit, "")
       def deleteButton = Button(Button.Props(delete (item,p.deleteLine), addStyles = Seq(bss.pullRight, bss.buttonXS,
                             bss.buttonOpt(CommonStyle.danger))), Icon.trashO, "")
-       val style = bss.listGroup
        val defaultL = LinePurchaseOrder()
        val cond = item.tid==s.item.getOrElse(defaultL).tid
-       //log.debug(s"  Condition  ${cond}")
-       val stylex = if(cond) style.itemOpt(CommonStyle.warning) else style.itemOpt(CommonStyle.success)
+       val style = if(cond) bss.listGroup.itemOpt(CommonStyle.warning) else bss.listGroup.itemOpt(CommonStyle.success)
 
-      <.li( stylex,
+      <.li( style,
         ^.fontSize:=12.px, ^.fontWeight:=50.px, ^.maxHeight:=30.px, ^.height:=30.px, ^.alignContent:="center", ^.tableLayout:="fixed",
         <.span(deleteButton , ^.alignContent:="left"),
-        <.span(item.id,^.paddingLeft:=10.px),
-        <.span(item.account ,  ^.minWidth := 80, ^.padding:=30.px),
-        <.span(item.side ,^.paddingLeft:=10.px),
-        <.span(item.oaccount ,^.paddingLeft:=10.px),
+        <.span(item.id,^.padding:=10.px),
+        <.span(item.account, ^.padding:=20.px),
+        <.input.checkbox(^.checked := item.side, ^.padding:=10.px),
+        <.span(item.oaccount ,^.padding:=20.px),
         <.span("%06.2f".format(item.amount.bigDecimal),^.paddingLeft:=10.px),
-        <.span( Moment(item.duedate.get.getTime).format("DD.MM.YYYY"),^.paddingLeft:=10),
-        <.span(item.text ,^.paddingLeft:=10.px),
+        <.span( Moment(item.duedate.get.getTime).format("DD.MM.YYYY"),^.paddingLeft:=10, ^.paddingRight:=30),
+        <.span(item.text, ^.paddingRight:=5),
         <.span(editButton,^.alignContent:="center")
       )
     }
