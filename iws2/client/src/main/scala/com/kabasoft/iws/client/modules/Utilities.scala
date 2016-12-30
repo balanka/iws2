ea7e37c8383c80ca95fe8cd13cb10fb0654f71b6
mady <batexy@gmail.com>
2016-12-30 16:52:54 +0100

    minor refactoring

--- iws2/client/src/main/scala/com/kabasoft/iws/client/modules/Utilities.scala
+++ iws2/client/src/main/scala/com/kabasoft/iws/client/modules/Utilities.scala
@@ -2,9 +2,16 @@ package com.kabasoft.iws.client.modules
 
 import japgolly.scalajs.react._
 import com.kabasoft.iws.gui.logger._
+import com.kabasoft.iws.gui.macros.Bootstrap.Button
+import com.kabasoft.iws.gui.macros.{Delete, Icon, Update}
+import com.kabasoft.iws.gui.services.IWSCircuit
+import com.kabasoft.iws.shared.IWS
+import scalacss.ScalaCssReact._
 
 
 object Utilities {
+
+  case class Statex [A](item: Option[A] = None)
   def updateOid1(idx: String, bs: BackendScope[Props, State]) = {
     // val oId = idx.substring(0, idx.indexOf("|"))
     //  log.debug(s"oid is "+oId)
@@ -37,5 +44,22 @@ object Utilities {
         s.item
       }
     ))
+  def edit[A<:IWS](itemx:Option[A], bs: BackendScope[Props, Statex[A]]) = bs.modState(s => s.copy(item = itemx))
 
+
+  def editedx[A<:IWS](itemx:A, bs: BackendScope[Props, Statex[A]]) = {
+    //bs.modState(s => s.copy(item = itemx))
+    edit(Some(itemx), bs)
+    Callback {IWSCircuit.dispatch(Update(itemx))}
+  }
+  def delete [A<:IWS] (item:A, bs: BackendScope[Props, Statex[A]]) = {
+    Callback.log("VendorInvoice deleted>>>>> ${item}")
+    Callback {IWSCircuit.dispatch(Delete(item))}
+    //bs.props >>= (_.proxy.dispatch(Delete(item)))
+  }
+  def newButton[A<:IWS](item:A, css:Seq[scalacss.StyleA], title:String,  bs: BackendScope[Props, Statex[A]])=
+    Button(Button.Props(edit(Some(item),bs), addStyles = css), Icon.plusSquare, title)
+
+  def saveButton[A<:IWS](item:A, css:Seq[scalacss.StyleA], title:String,  bs: BackendScope[Props, Statex[A]]) =
+    Button(Button.Props(editedx(item, bs), addStyles = css), Icon.circleO, title)
 }
